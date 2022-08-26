8f70ac432fae7c358d59860abdaf53677a359adc
adinak <adinak@campus.technion.ac.il>
2022-08-26 12:40:46 +0300

    getting started

---
+++ brick-breaker/src/App.js
@@ -0,0 +1,17 @@
+"use-strict";
+
+import './App.css';
+import BoardGame from './BoardGame.react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BoardGame></BoardGame>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ brick-breaker/src/BoardGame.react.tsx
@@ -0,0 +1,12 @@
+"use-strict";
+
+import React from 'react';
+import './BoardGame.css'
+
+type Props = Readonly<{}>;
+
+export default function BoardGame(props: Props): React.ReactNode {
+    
+    return <div className='board'></div>;
+}
+
